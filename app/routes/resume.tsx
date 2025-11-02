f31bcbaa7ea522f7ba42b27e52bd33c67cc93ee1
Ansh <jndl.ansh@gmail.com>
2025-11-02 12:23:25 +0530

    Completed the feedback section for our Analyzer

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import {usePuterStore} from "~/lib/puter";
 import {Link, useNavigate, useParams} from "react-router";
+import Summary from "~/components/Summary";
+import Details from "~/components/Details";
+import ATS from "~/components/ATS";
 
 export const meta = () => ([
     {title: 'Resumind | Review '},
@@ -12,7 +15,7 @@ const Resume = () => {
     const {id} = useParams();
     const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
-    const [feedback, setFeedback] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -69,11 +72,15 @@ const Resume = () => {
                 </section>
                 <section className="feedback-section">
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
-                    {/*{feedback ? (*/}
-                    {/*    <div className="flex flex-col gap-8 animate-in fade-in duration-1000">*/}
-                    {/*        */}
-                    {/*    </div>*/}
-                    {/*)}*/}
+                    {feedback ? (
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+                            <Summary feedback={feedback}/>
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <Details feedback={feedback}/>
+                        </div>
+                    ):(
+                        <img src="/images/resume-scan-2.gif" className="w-full" />
+                    )}
                 </section>
             </div>
         </main>
